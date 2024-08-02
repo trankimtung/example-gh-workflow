6006f292b96692a206715eaf01fec940f6489e1e
Tran Kim Tung <contact@trankimtung.com>
2024-08-02 17:37:18 +0800

    test: add an example test case

---
+++ src/test/java/com/trankimtung/example/hello/MainTest.java
@@ -0,0 +1,13 @@
+package com.trankimtung.example.hello;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void shouldAlwaysSucceed() {
+        assertTrue(true); // for demo purpose
+    }
+}
